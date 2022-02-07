ed5eaaa4d96cba2c35da88c0958e5ee745691030
Eik Hvattum Røgeberg <eik.rogeberg@gmail.com>
2022-02-07 11:30:52 +0100

    replace backend Java template with Kotlin template

---
+++ backend/src/main/kotlin/com/groupup/GroupUp/GroupUpApplication.kt
@@ -0,0 +1,11 @@
+package com.groupup.GroupUp
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GroupUpApplication
+
+fun main(args: Array<String>) {
+	runApplication<GroupUpApplication>(*args)
+}
---
+++ backend/src/test/kotlin/com/groupup/GroupUp/GroupUpApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.groupup.GroupUp
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GroupUpApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
