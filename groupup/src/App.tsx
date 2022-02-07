bee52ed68f329ee0f5feeec7d03547a0402d954f
Olav Kihle <olavki@stud.ntnu.no>
2022-02-07 11:22:45 +0100

    Setup react webapp, backend in separate folder

---
+++ groupup/src/App.tsx
@@ -0,0 +1,15 @@
+import Typography from '@mui/material/Typography';
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Typography variant="h3">
+        Create GroupUp App 
+      </Typography>
+    </div>
+  );
+}
+
+export default App;
