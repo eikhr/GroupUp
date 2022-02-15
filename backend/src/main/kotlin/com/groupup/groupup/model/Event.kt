18b3c0463a1ff99100b938fca82d4c1726399047
ingamd <ingamd@stud.ntnu.no>
2022-02-15 13:24:38 +0100

    Added default values for null safety in Event.kt Co-authored-by: "Olav Kihle" olavki@stud.ntnu.no

--- backend/src/main/kotlin/com/groupup/groupup/model/Event.kt
+++ backend/src/main/kotlin/com/groupup/groupup/model/Event.kt
@@ -1,13 +1,19 @@
 package com.groupup.groupup.model
 
 import org.springframework.format.annotation.DateTimeFormat
-import java.util.Date
+import java.util.GregorianCalendar
 import javax.persistence.Column
 import javax.persistence.Entity
 import javax.persistence.GeneratedValue
 import javax.persistence.GenerationType
 import javax.persistence.Id
 import javax.persistence.Table
+
+private const val DefaultYear = 2022
+
+private const val DefaultMonth = 4
+
+private const val DefaultDay = 13
 
 @Entity
 @Table(name = "events")
@@ -17,10 +23,10 @@ open class Event {
     @Column(name = "id", nullable = false)
     open var id: Long? = null
     @Column
-    open lateinit var title: String
+    open var title: String = ""
     @Column
-    open lateinit var description: String
+    open var description: String = ""
     @Column
     @DateTimeFormat
-    open lateinit var date: Date
+    open var date: GregorianCalendar = GregorianCalendar(DefaultYear, DefaultMonth, DefaultDay)
 }
