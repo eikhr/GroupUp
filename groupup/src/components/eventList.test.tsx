4b9a3de11df9fc3ccd0a584ec5cbd17ef42bd17a
Sophie Feldberg Wattne <sophiefw@dhcp-10-22-5-118.wlan.ntnu.no>
2022-02-17 13:03:25 +0100

    Get data from API request Co-authored-by: Eik Hvattum Røgeerg <eikhr@stud.ntnu.no>

--- groupup/src/components/eventList.test.tsx
+++ groupup/src/components/eventList.test.tsx
@@ -1,8 +1,9 @@
 import { render } from '@testing-library/react'
 import EventList from './eventList'
 import React from 'react'
+import { act } from 'react-dom/test-utils'
 
-it('renders a list events', () => {
+it('renders a list events', async () => {
   const events = [
     { title: 'testevent', id: 1, description: 'Whatever' },
     { title: 'testevent', id: 2, description: 'Whatever' },
@@ -15,8 +16,18 @@ it('renders a list events', () => {
     { title: 'testevent', id: 9, description: 'Whatever' },
     { title: 'testevent', id: 10, description: 'Whatever' },
   ]
-  const { container } = render(<EventList events={events} />)
+  jest.spyOn(global, 'fetch').mockImplementation(() =>
+    Promise.resolve({
+      json: () => Promise.resolve(events),
+    } as any)
+  )
+  let container: HTMLElement
+  await act(async () => {
+    container = render(<EventList />).container
+  })
+
   for (const event of events) {
     expect(container.querySelector(`[data-testid='event-${event.id}']`)).not.toBeNull()
   }
+  global.fetch.mockRestore()
 })
--- groupup/src/components/eventList.tsx
+++ groupup/src/components/eventList.tsx
@@ -1,16 +1,21 @@
 import { Grid } from '@mui/material'
 import IEvent from '../models/event'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import EventCard from './eventCard'
 
-interface IProps {
-  events: IEvent[]
-}
+const EventList = () => {
+  const [events, setEvents] = useState<IEvent[] | null>(null)
+  async function fetchEvents() {
+    const response = await fetch('/')
+    setEvents(await response.json())
+  }
+  useEffect(() => {
+    fetchEvents()
+  }, [])
 
-const EventList = (props: IProps) => {
   return (
     <Grid container spacing={2} justifyContent="center">
-      {props.events.map((event) => (
+      {events?.map((event) => (
         <Grid item key={event.id}>
           <EventCard data={event} />
         </Grid>
