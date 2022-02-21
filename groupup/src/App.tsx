20d219f1abf1a88080671834ed7b0c2104f2294d
Eik Hvattum Røgeberg <eik.rogeberg@gmail.com>
2022-02-21 21:52:00 +0100

    Add router

--- groupup/src/App.tsx
+++ groupup/src/App.tsx
@@ -1,15 +1,20 @@
-import Typography from '@mui/material/Typography'
 import React from 'react'
 import './App.css'
 import EventList from './components/event/eventList'
-import Form from './components/event/createEvent'
+import { Routes, Route } from 'react-router-dom'
+import CoverPage from './components/coverPage'
+import Header from './components/Header'
+import CreateEvent from "./components/event/createEvent";
 
 const App = () => {
   return (
     <div className="App">
-      <Typography variant="h3">Create GroupUp App</Typography>
-      <EventList />
-      <Form />
+      <Header />
+      <Routes>
+        <Route path="/" element={<CoverPage />} />
+        <Route path="/events" element={<EventList />} />
+        <Route path="/addEvent" element={<CreateEvent />} />
+      </Routes>
     </div>
   )
 }
