87521230d4900b4d4824ef92ab9ad538dca21dd9
Eik Hvattum Røgeberg <eik.rogeberg@gmail.com>
2022-02-22 13:40:04 +0100

    Add test for getting list of events

--- backend/src/test/kotlin/com/groupup/groupup/EventControllerTest.kt
+++ backend/src/test/kotlin/com/groupup/groupup/EventControllerTest.kt
@@ -6,9 +6,12 @@ import com.groupup.groupup.controller.EventController
 import com.groupup.groupup.model.Event
 import com.groupup.groupup.repository.EventRepository
 import com.groupup.groupup.service.EventService
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertNotNull
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 import org.mockito.Mockito
+import org.mockito.Mockito.times
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
@@ -20,6 +23,7 @@ import org.springframework.test.web.servlet.MvcResult
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers
 import java.util.GregorianCalendar
+import kotlin.random.Random
 
 @ContextConfiguration(classes = [EventController::class, EventRepository::class])
 @AutoConfigureMockMvc
@@ -44,7 +48,8 @@ class EventControllerTest {
         return url.toString()
     }
 
-    private fun getEvents(): Collection<Event> {
+    private fun getEvents(events: Collection<Event>): Collection<Event> {
+        Mockito.doReturn(events).`when`(eventService).getEvents()
         val result: MvcResult = mockMvc.perform(
             MockMvcRequestBuilders
                 .get(apiUrl("events"))
@@ -69,12 +74,44 @@ class EventControllerTest {
         )
             .andExpect(MockMvcResultMatchers.status().isOk)
             .andReturn()
+        Mockito.verify(eventService, times(1)).createEvent(anyEvent())
+    }
+
+    private fun createTestEvent(): Event {
+        val event = Event()
+        event.title = "Test event " + Random.nextFloat().toString()
+        event.description = "description " + Random.nextFloat().toString()
+        event.date = GregorianCalendar(
+            Random.nextInt(2022, 2030),
+            Random.nextInt(0, 11),
+            Random.nextInt(1, 29),
+            Random.nextInt(23),
+            Random.nextInt(59),
+            Random.nextInt(59)
+        )
+        return event
+    }
+
+    private fun assertEventCollectionsEqual(expected: Collection<Event>, actual: Collection<Event>) {
+        for (expectedEvent in expected) {
+            val actualEvent = actual.find { it.title == expectedEvent.title }
+            assertNotNull(actualEvent)
+            assertEquals(expectedEvent.description, actualEvent?.description)
+            assertEquals(expectedEvent.date.time, actualEvent?.date?.time)
+        }
     }
 
     @Test
     fun testGetEmptyEventsList() {
-        val events: Collection<Event> = getEvents()
+        val events: Collection<Event> = getEvents(ArrayList())
         assertTrue(events.isEmpty())
+    }
+
+    @Test
+    fun testGetEventsList() {
+        val testEvents = listOf(createTestEvent(), createTestEvent(), createTestEvent())
+        val events: Collection<Event> = getEvents(testEvents)
+        assertEventCollectionsEqual(testEvents, events)
     }
 
     @Test
@@ -84,6 +121,5 @@ class EventControllerTest {
         event.description = "description"
         event.date = GregorianCalendar(2022, 4, 13, 12, 12, 12)
         addEvent(event)
-        println(getEvents())
     }
 }
