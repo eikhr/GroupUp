a27ae157b8f3f83274e4328c429789ad6701bbf4
ingamd <ingamd@stud.ntnu.no>
2022-02-24 14:24:04 +0100

    CreateGroup can now select related interests. Some files also edited by prettier
    Co-authored-by: eikhr <eikhr@stud.ntnu.no>

--- groupup/src/components/groups/createGroup.tsx
+++ groupup/src/components/groups/createGroup.tsx
@@ -1,14 +1,31 @@
-import { Button, Grid, Stack, TextField, Typography } from '@mui/material'
+import {
+  Button,
+  Checkbox,
+  FormControl,
+  Grid,
+  InputLabel,
+  ListItemText,
+  MenuItem,
+  OutlinedInput,
+  Select,
+  SelectChangeEvent,
+  Stack,
+  TextField,
+  Typography,
+} from '@mui/material'
 import React, { ChangeEvent, FormEvent, useState } from 'react'
+import { Favorite, FavoriteBorder } from '@mui/icons-material'
 
 const defaultValues = {
   name: '',
   description: '',
   interests: [''],
 }
+const interestList = ['hiking', 'parties', 'horses', 'pokemon go']
 
 const Form = () => {
   const [formValues, setFormValues] = useState(defaultValues)
+  const [interests, setInterest] = React.useState<string[]>([])
   const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target
     setFormValues({
@@ -17,6 +34,11 @@ const Form = () => {
     })
   }
 
+  const handleSelectChange = (e: SelectChangeEvent<typeof interests>) => {
+    const { value } = e.target
+    setInterest([...value])
+    console.log(value)
+  }
   const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault()
   }
@@ -44,6 +66,31 @@ const Form = () => {
             value={formValues.description}
             onChange={handleInputChange}
           />
+          <FormControl>
+            <InputLabel id="interest-label">Interests</InputLabel>
+            <Select
+              labelId="interest-label"
+              label="Pick interests"
+              name="interests"
+              multiple
+              displayEmpty
+              value={interests}
+              onChange={handleSelectChange}
+              input={<OutlinedInput label="Interests" />}
+              renderValue={(selected) => selected.join(', ')}
+            >
+              {interestList.map((interest) => (
+                <MenuItem key={interest} value={interest}>
+                  <Checkbox
+                    icon={<FavoriteBorder />}
+                    checkedIcon={<Favorite />}
+                    checked={interests.indexOf(interest) > -1}
+                  />
+                  <ListItemText primary={interest} />
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
           <Button variant="contained" color="primary" type="submit">
             Submit
           </Button>
