1d33854e5b571943f87410999e3229476c670855
Eik Hvattum Røgeberg <eik.rogeberg@gmail.com>
2022-02-24 10:40:11 +0100

    minor style changes in CoverPage

--- groupup/src/components/coverPage.tsx
+++ groupup/src/components/coverPage.tsx
@@ -3,29 +3,29 @@ import { Button, Grid, Stack, Typography } from '@mui/material'
 
 const CoverPage = () => {
   return (
-    <Grid container>
-      <Grid item xs={12} md={4} alignItems="center">
-        <Stack height={1} justifyContent="center">
+    <Grid container sx={{ maxWidth: 1200, margin: 'auto' }}>
+      <Grid item xs={12} md={5} alignItems="center">
+        <Stack height={1} justifyContent="center" alignItems="center">
           <Typography data-testid="title" variant="h1">
             GroupUp
           </Typography>
-          <Typography variant="h5">Utvid vennekretsen!</Typography>
+          <Typography gutterBottom variant="h5">
+            Utvid vennekretsen!
+          </Typography>
+          <Button component="a" href="/events" variant="outlined">
+            Start
+          </Button>
         </Stack>
       </Grid>
-      <Grid item xs={12} md={8}>
+      <Grid item xs={12} md={7}>
         <Stack>
           <img src={'/group.png'} alt="group of friends" />
-          <Typography variant="caption">
+          <Typography variant="caption" sx={{ fontSize: 8 }}>
             {
               'Sara 506, CC BY-SA 3.0 <https://creativecommons.org/licenses/by-sa/3.0>, via Wikimedia Commons. Changed white background to transparent.'
             }
           </Typography>
         </Stack>
-      </Grid>
-      <Grid item xs={12}>
-        <Button component="a" href="/events" variant="outlined">
-          Enter
-        </Button>
       </Grid>
     </Grid>
   )
