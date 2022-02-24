e8892770ef72e3aa3ab675fd9b784d8c11ac19c5
Eik Hvattum Røgeberg <eik.rogeberg@gmail.com>
2022-02-24 11:05:04 +0100

    add API endpoint for adding event in frontend

--- groupup/src/API/index.ts
+++ groupup/src/API/index.ts
@@ -1,10 +1,22 @@
 import IEvent from '../models/event'
+import { json } from "stream/consumers";
 
 const baseUrl = 'http://localhost:8080/api'
 
 export interface APIError {
   message: string
   status: number
+}
+
+const doRequest = async <T>(url: string, options: RequestInit): Promise<T> => {
+  const response = await fetch(url, options)
+
+  // The request was sent successfully
+  if (response.status >= 200 && response.status < 300) {
+    return response.json()
+  } else {
+    throw { message: 'Request returned non-2xx response code', status: response.status }
+  }
 }
 
 const API = {
@@ -15,15 +27,18 @@ const API = {
       method: 'GET',
     }
 
-    const response = await fetch(url, options)
-
-    // The request was sent successfully
-    if (response.status >= 200 && response.status < 300) {
-      return response.json()
-    } else {
-      throw { message: 'Could not fetch the events', status: response.status }
-    }
+    return await doRequest(url, options);
   },
+  addEvent: async (event: IEvent): Promise<IEvent> => {
+    const url = baseUrl + '/events/add'
+
+    const options: RequestInit = {
+      method: 'POST',
+      body: JSON.stringify(event),
+    }
+
+    return await doRequest(url, options);
+  }
 }
 
 export default API
