10b8a7da011a297d786947915a6e7d0495599401
Eik Hvattum Røgeberg <eik.rogeberg@gmail.com>
2022-03-01 12:36:16 +0100

    Change information on event card

--- groupup/src/components/event/eventCard.tsx
+++ groupup/src/components/event/eventCard.tsx
@@ -1,5 +1,6 @@
-import { Card, CardContent, Typography } from '@mui/material'
+import { Card, CardContent, Chip, Stack, Typography } from "@mui/material";
 import moment from 'moment'
+import 'moment/locale/nb'
 import React from 'react'
 import IEvent from '../../models/event'
 
@@ -7,28 +8,31 @@ interface IProps {
   data: IEvent
 }
 const EventCard = (props: IProps) => {
+  moment.locale("nb")
+
   const groups = props.data.groups
   const arrangingGroup = groups ? groups[0] : undefined
   return (
     <Card data-testid={'event-' + props.data.id} sx={{ width: 345 }}>
       <CardContent>
+        {props.data.date && (
+          <Typography data-testid="time" sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+            {moment(props.data.date).calendar({sameElse: "dddd Do MMMM [kl.] HH:mm"})}
+          </Typography>
+        )}
         <Typography data-testid="title" gutterBottom variant="h5" component="div">
           {props.data.title}
         </Typography>
         <Typography data-testid="description" variant="body2" color="text.secondary">
           {props.data.description}
         </Typography>
-        {props.data.date && (
-          <Typography data-testid="time" variant="body2" color="text.secondary">
-            {moment(props.data.date).format('LLLL')}
-          </Typography>
-        )}
         <Typography data-testid="groups" variant="body2" color="text.secondary">
           {arrangingGroup?.name}
         </Typography>
         <Typography data-testid="groupsEmail" variant="body2" color="text.secondary">
           {arrangingGroup?.email}
         </Typography>
+        <Stack direction="row" spacing={1}>{arrangingGroup?.interests?.map(interest => (<Chip key={interest} label={interest}/>))}</Stack>
       </CardContent>
     </Card>
   )
