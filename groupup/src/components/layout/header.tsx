9f1e53045dd26838999add6db94ab305b9426526
Eik Hvattum Røgeberg <eik.rogeberg@gmail.com>
2022-03-10 13:24:55 +0100

    Add page for choosing current group
    Co-authored-by: Christoffer Netskar <chrisnet@stud.ntnu.no>

--- groupup/src/components/layout/header.tsx
+++ groupup/src/components/layout/header.tsx
@@ -1,18 +1,29 @@
-import React from 'react'
-import { AppBar, Button, Toolbar } from '@mui/material'
+import React, { useContext } from 'react'
+import { AppBar, Box, Button, Container, Toolbar } from '@mui/material'
 import Logo from './logo'
+import CurrentGroupContext from '../../context/CurrentGroupContext'
 
 const Header = () => {
+  const { currentGroup } = useContext(CurrentGroupContext)
+
   return (
     <>
       <AppBar sx={{ height: 64, alignItems: 'flex-start' }}>
-        <Button
-          variant="outlined"
-          href="/"
-          sx={{ height: 1, width: 'auto', color: 'primary.contrastText' }}
-        >
-          <Logo sx={{ height: 1, textTransform: 'none' }} />
-        </Button>
+        <Container maxWidth="xl">
+          <Toolbar disableGutters>
+            <Button
+              variant="outlined"
+              href="/"
+              sx={{ height: 1, width: 'auto', color: 'primary.contrastText' }}
+            >
+              <Logo sx={{ height: 1, textTransform: 'none' }} />
+            </Button>
+            <Box sx={{ flexGrow: 1 }} />
+            <Button sx={{ color: 'primary.contrastText' }}>
+              {currentGroup ? currentGroup.name : 'No group :('}
+            </Button>
+          </Toolbar>
+        </Container>
       </AppBar>
       <Toolbar />
     </>
