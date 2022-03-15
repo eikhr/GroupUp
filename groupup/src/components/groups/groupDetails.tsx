257c2053bb035724d5466ebb16843d8508db66ee
ingamd <ingamd@stud.ntnu.no>
2022-03-15 14:34:03 +0100

    GroupDetails now displays a button for buying gold if current group is not already a gold member

--- groupup/src/components/groups/groupDetails.tsx
+++ groupup/src/components/groups/groupDetails.tsx
@@ -1,4 +1,12 @@
-import { Card, CardContent, CardMedia, Chip, Stack, Typography } from '@mui/material'
+import {
+  Button,
+  Card,
+  CardContent,
+  CardMedia,
+  Chip,
+  Stack,
+  Typography,
+} from '@mui/material'
 import React, { useContext } from 'react'
 import EventIcon from '@mui/icons-material/Event'
 import InterestsIcon from '@mui/icons-material/Interests'
@@ -14,49 +22,58 @@ const GroupDetails = () => {
   }
 
   return (
-    <Card>
-      <CardMedia
-        component="img"
-        height="200"
-        src={'/groupMy.jpg'}
-        alt="Portrait of ASTP crews - restoration.jpg"
-      />
-      <CardContent>
-        <Typography data-testid="title" gutterBottom variant="h5" component="div">
-          {currentGroup.name}
-        </Typography>
-        <Typography
-          data-testid="description"
-          gutterBottom
-          variant="body2"
-          color="text.secondary"
-        >
-          {currentGroup.description}
-        </Typography>
-
-        <Typography
-          data-testid="groupsEmail"
-          variant="body2"
-          color="text.secondary"
-          gutterBottom
-        >
-          {currentGroup.contactEmail}
-        </Typography>
-        <Stack direction="row" spacing={1} sx={{ mt: 2 }}>
-          {currentGroup.interests?.map((interest) => (
-            <Chip icon={<InterestsIcon />} key={interest} label={interest} />
-          ))}
-        </Stack>
-        <Stack data-testid="description" spacing={1} direction={'row'} sx={{ mt: 2 }}>
-          <Typography variant="body2" color="text">
-            Aktiviteter:{' '}
+    <Stack spacing={1}>
+      <Button
+        sx={{ background: !currentGroup?.gold ? '#DAA520' : undefined }}
+        size="large"
+        variant="contained"
+      >
+        Get GroupUp GOLD!
+      </Button>
+      <Card>
+        <CardMedia
+          component="img"
+          height="200"
+          src={'/groupMy.jpg'}
+          alt="Portrait of ASTP crews - restoration.jpg"
+        />
+        <CardContent>
+          <Typography data-testid="title" gutterBottom variant="h5" component="div">
+            {currentGroup.name}
           </Typography>
-          {currentGroup.events?.map((event) => (
-            <Chip icon={<EventIcon />} key={event.title} label={event.title} />
-          ))}
-        </Stack>
-      </CardContent>
-    </Card>
+          <Typography
+            data-testid="description"
+            gutterBottom
+            variant="body2"
+            color="text.secondary"
+          >
+            {currentGroup.description}
+          </Typography>
+
+          <Typography
+            data-testid="groupsEmail"
+            variant="body2"
+            color="text.secondary"
+            gutterBottom
+          >
+            {currentGroup.contactEmail}
+          </Typography>
+          <Stack direction="row" spacing={1} sx={{ mt: 2 }}>
+            {currentGroup.interests?.map((interest) => (
+              <Chip icon={<InterestsIcon />} key={interest} label={interest} />
+            ))}
+          </Stack>
+          <Stack data-testid="description" spacing={1} direction={'row'} sx={{ mt: 2 }}>
+            <Typography variant="body2" color="text">
+              Aktiviteter:{' '}
+            </Typography>
+            {currentGroup.events?.map((event) => (
+              <Chip icon={<EventIcon />} key={event.title} label={event.title} />
+            ))}
+          </Stack>
+        </CardContent>
+      </Card>
+    </Stack>
   )
 }
 
