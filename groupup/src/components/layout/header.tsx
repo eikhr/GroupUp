8c26f9cb0f3bca683021e14003ea8e557e08f33d
Sophie Feldberg Wattne <sophiefw@stud.ntnu.no>
2022-03-15 13:17:56 +0100

    Change colour and logo if current group has gold Co-authored -by: Eik Hvattum Røgeberg <eikhr@stud.ntnuno>

--- groupup/src/components/layout/header.tsx
+++ groupup/src/components/layout/header.tsx
@@ -8,15 +8,21 @@ const Header = () => {
 
   return (
     <>
-      <AppBar sx={{ height: 64, alignItems: 'flex-start' }}>
+      <AppBar
+        sx={{
+          height: 64,
+          alignItems: 'flex-start',
+          background: currentGroup?.gold ? '#DAA520' : undefined,
+        }}
+      >
         <Container maxWidth="xl">
           <Toolbar disableGutters>
             <Button
-              variant="outlined"
+              variant="text"
               href="/"
               sx={{ height: 1, width: 'auto', color: 'primary.contrastText' }}
             >
-              <Logo sx={{ height: 1, textTransform: 'none' }} />
+              <Logo sx={{ height: 1, textTransform: 'none' }} gold={currentGroup?.gold} />
             </Button>
             <Box sx={{ flexGrow: 1 }} />
             <Button sx={{ color: 'primary.contrastText' }}>
--- groupup/src/components/layout/logo.tsx
+++ groupup/src/components/layout/logo.tsx
@@ -4,13 +4,14 @@ import { Stack, SxProps, Typography } from '@mui/material'
 
 interface IProps {
   sx?: SxProps
+  gold?: boolean
 }
 
 const Logo = (props: IProps) => {
   return (
     <Stack direction="row" alignItems="center" sx={props.sx}>
       <GroupsRoundedIcon sx={{ p: 1, height: 1 }} />
-      <Typography variant="h5">GroupUp</Typography>
+      <Typography variant="h5">{props.gold ? 'GroupUp Gold' : 'GroupUp'}</Typography>
     </Stack>
   )
 }
