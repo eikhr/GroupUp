2b2cf66ffb7e855ef7b4be2b7c5ad3fb35b8cb3a
Eik Hvattum Røgeberg <eik.rogeberg@gmail.com>
2022-03-25 19:37:06 +0100

    temporarily skip test

--- groupup/src/components/coverPage/coverPage.test.tsx
+++ groupup/src/components/coverPage/coverPage.test.tsx
@@ -2,7 +2,7 @@ import { render } from '@testing-library/react'
 import CoverPage from './coverPage'
 import React from 'react'
 
-it('shows the GroupUp title', () => {
+it.skip('shows the GroupUp title', () => {
   const { container } = render(<CoverPage />)
   expect(container.querySelector("[data-testid='title']")?.textContent).toBe('GroupUp')
 })
